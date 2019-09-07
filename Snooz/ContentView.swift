81b6c5fdaa0bf4f8d9143f9cba618b0d90b00ac4
Joss Manger <jossferatu@gmail.com>
2019-09-07 10:29:52 -0400

    Initial Commit

---
+++ Snooz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snooz
+//
+//  Created by Joss Manger on 9/7/19.
+//  Copyright © 2019 Joss Manger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
