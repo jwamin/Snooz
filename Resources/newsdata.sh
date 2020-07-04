4f60bd03beac5cdfdb5fdf4ea49ab2cf95210369
Joss Manger <jossferatu@gmail.com>
2020-07-03 21:12:47 -0400

    adds sh script and launchd definition for collecting news info periodically over the course of the day, adds ignorefile

---
+++ Resources/newsdata.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+#Shell Script to capture data from NewsAPI
+#=========================================
+# By Jocelyn Manger
+
+#Will Run on schduled jobs at:
+#6am
+#12pm
+#6pm
+#12am
+say "starting launchd run" #helps to debug background process!
+echo "Running Training Data Capture"
+echo "Downloading Headlines for $(date)"
+
+CURRENT_DATE=$(date +"%Y-%m-%d-%I-%M%p")
+OUTPUT_FILE="/Users/jwamin/sandbox.nosync/beta-projects-2019-20/Snooz/Resources/out/news-headlines-${CURRENT_DATE}.json"
+
+curl https://newsapi.org/v2/top-headlines\?country\=us\&apiKey\=510332d2557240f496c74ee5687e547e \
+| python3 -m json.tool \
+> $OUTPUT_FILE
+
+echo "${OUTPUT_FILE} created."
