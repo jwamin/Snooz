5ce7ce1d389f65b8e356af711a1f980e1d3b507e
Joss Manger <jossferatu@gmail.com>
2020-07-23 19:20:33 -0400

    updates news capture script

--- Resources/newsdata.sh
+++ Resources/newsdata.sh
@@ -9,12 +9,12 @@
 #12pm
 #6pm
 #12am
-say "starting launchd run" #helps to debug background process!
+say "running launchd task" #helps to debug background process!
 echo "Running Training Data Capture"
 echo "Downloading Headlines for $(date)"
 
 CURRENT_DATE=$(date +"%Y-%m-%d-%I-%M%p")
-OUTPUT_FILE="/Users/jwamin/sandbox.nosync/beta-projects-2019-20/Snooz/Resources/out/news-headlines-${CURRENT_DATE}.json"
+OUTPUT_FILE="/Users/jwamin/sandbox.nosync/beta-projects-20-21/Snooz/Resources/out/news-headlines-${CURRENT_DATE}.json"
 
 curl https://newsapi.org/v2/top-headlines\?country\=us\&apiKey\=510332d2557240f496c74ee5687e547e \
 | python3 -m json.tool \
